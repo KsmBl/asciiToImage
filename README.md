e92661cf81f8b9d3920b2f6ed0c2432266e90530
KsmBl <katzen.sind.lecker69@gmail.com>
2025-08-05 14:29:18 +0200

    custom colors

--- README.md
+++ README.md
@@ -13,3 +13,15 @@ sets filename and type (png, jpg...)
 
 > -s 34  
 sets font size, higher fontsize => higher resolutuion  
+
+> -fc red  
+sets font color to red  
+
+> -fc "#123456"  
+sets font color to hexcode #123456  
+
+> -bc red  
+sets background color to red  
+
+> -bc "#123456"  
+sets background color to hexcode #123456  
--- main.py
+++ main.py
@@ -8,12 +8,18 @@ args = sys.argv
 
 outputFile="output.png"
 fontSize = 14
+fontColor = "white"
+backgroundColor = "black"
 
 for i in range(len(args)):
 	if args[i] == "-o" and i+1 < len(args):
 		outputFile = args[i + 1]
 	elif args[i] == "-s" and i+1 < len(args):
 		fontSize = int(args[i + 1])
+	elif args[i] == "-fc" and i+1 < len(args):
+		fontColor = str(args[i + 1])
+	elif args[i] == "-bc" and i+1 < len(args):
+		backgroundColor = str(args[i + 1])
 
 asciiInput = sys.stdin.read()
 
@@ -22,18 +28,18 @@ lines = asciiInput.splitlines()
 fontPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Anonymous.ttf")
 font = ImageFont.truetype(fontPath, fontSize)
 
-char_width = font.getlength("A")
+charWidth = font.getlength("A")
 ascent, descent = font.getmetrics()
 lineHeight = (ascent + descent)
 
-imgWidth = int(char_width * max(len(line) for line in lines))
+imgWidth = int(charWidth * max(len(line) for line in lines))
 imgHeight = lineHeight * len(lines)
 
-image = Image.new("RGB", (imgWidth, imgHeight), color="black")
+image = Image.new("RGB", (imgWidth, imgHeight), color=backgroundColor)
 draw = ImageDraw.Draw(image)
 
 for i, line in enumerate(lines):
-	draw.text((0, i * lineHeight), line, fill="white", font=font)
+	draw.text((0, i * lineHeight), line, fill=fontColor, font=font)
 
 image.save(outputFile)
 print(f"saved as {outputFile}")
