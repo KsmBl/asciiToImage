a52f063937d6a8dd9beb51c45db87b371927a1c3
KsmBl <katzen.sind.lecker69@gmail.com>
2025-08-05 12:24:13 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+from PIL import Image, ImageDraw, ImageFont
+import sys
+import os
+
+args = sys.argv
+
+outputFile="output.png"
+fontSize = 14
+
+for i in range(len(args)):
+	if args[i] == "-o" and i+1 < len(args):
+		outputFile = args[i + 1]
+	elif args[i] == "-s" and i+1 < len(args):
+		fontSize = int(args[i + 1])
+
+asciiInput = sys.stdin.read()
+
+lines = asciiInput.splitlines()
+
+fontPath = os.path.join(os.path.dirname(__file__), "Anonymous.ttf")
+font = ImageFont.truetype(fontPath, fontSize)
+
+char_width = font.getlength("A")
+ascent, descent = font.getmetrics()
+lineHeight = (ascent + descent) + 10
+
+imgWidth = int(char_width * max(len(line) for line in lines))
+imgHeight = lineHeight * len(lines)
+
+image = Image.new("RGB", (imgWidth, imgHeight), color="black")
+draw = ImageDraw.Draw(image)
+
+for i, line in enumerate(lines):
+	draw.text((0, i * lineHeight), line, fill="white", font=font)
+
+image.save(outputFile)
+print(f"saved as {outputFile}")
